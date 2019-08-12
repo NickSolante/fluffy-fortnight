d9d5b6355d4ca2cfdc9175c8d1f1831934d4ef68
frostnovus <psolante@deakin.edu.au>
2019-08-12 21:13:46 +1000

    new

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,16 @@
 <template>
-  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  <div>
+    <SearchBar></SearchBar>
+  </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import SearchBar from "./components/SearchBar";
 
 export default {
   name: "app",
   components: {
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "todo one",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "todo two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "todo three",
-          completed: false
-        }
-      ]
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    }
+    SearchBar
   }
 };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: event => {
+      console.log(event.target.value);
+    }
+  }
+};
+</script>
+<style>
+</style>
